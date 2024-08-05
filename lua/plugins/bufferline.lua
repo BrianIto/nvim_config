a9cb391c50467d4eaba8273cece0276ff1037004
Brian <brian.moura@indt.org.br>
2024-08-05 09:04:17 -0400

    🐛 fix: update diagnostics_indicator function in bufferline config

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -17,13 +17,12 @@ return {
   opts = {
     options = {
      diagnostics = "nvim_lsp",
-      always_show_bufferline = true,
-      diagnostics_indicator = function(_, _, diag)
-        local ret = (diag.error and icons.Error .. diag.error .. " " or "")
-          .. (diag.warning and icons.Warn .. diag.warning or "")
-        return vim.trim(ret)
-      end,
-      offsets = {{ filetype = "NvimTree", text = "File Explorer", textAlign = "center"}},
+diagnostics_indicator = function(count, level, diagnostics_dict, context)
+  local icon = level:match("error") and " " or " "
+  return " " .. icon .. count
+end,
+     always_show_bufferline = true,
+     offsets = {{ filetype = "NvimTree", text = "File Explorer", textAlign = "center"}},
  
     },
  },
