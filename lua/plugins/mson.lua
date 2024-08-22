581dc6046e86588befec69dfe3f19b20db5479ee
Brian <brian.moura@indt.org.br>
2024-08-22 07:36:34 -0400

    ✨ feat: add mason-lspconfig.nvim plugin configuration

--- lua/plugins/mson.lua
+++ lua/plugins/mson.lua
@@ -5,13 +5,26 @@ return {
 		require("mason").setup()
 	end
 	},
-	 {
-        'neovim/nvim-lspconfig',
-        config = function()
-            local lspconfig = require('lspconfig')
-            lspconfig.lua_ls.setup {}
-            lspconfig.pyright.setup {}
-            lspconfig.tsserver.setup {}
-        end
-    },
+	{
+	'williamboman/mason-lspconfig.nvim',
+    	config = function()
+      		require('mason-lspconfig').setup({
+        		ensure_installed = { 'pyright', 'tsserver', 'clangd' },  -- Add the servers you want to install
+      		})
+    	end,
+	},
+  {
+    'neovim/nvim-lspconfig',
+    config = function()
+      local lspconfig = require('lspconfig')
+      local mason_lspconfig = require('mason-lspconfig')
+
+      mason_lspconfig.setup_handlers({
+        function(server_name)
+          lspconfig[server_name].setup({})
+        end,
+      })
+    end,
+  },
 }
+
