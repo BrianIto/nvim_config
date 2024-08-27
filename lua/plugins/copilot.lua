af1de7a834184b020e853b18772142384c49623e
Brian Ito <brian.moura@indt.org.br>
2024-08-26 22:56:59 -0400

    🎉 feat: add configuration for Copilot to disable tab mapping and set custom keymap

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,7 @@
 return {
-	"github/copilot.vim"
+	"github/copilot.vim",
+	config = function()
+		vim.g.copilot_no_tab_map = true
+		vim.api.nvim_set_keymap("i", "<leader><Tab>", "copilot#Accept('<CR>')", { expr=true, silent = true })
+	end,
 }
