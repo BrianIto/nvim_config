4a23303c2a60fa59b18bf895a277dfc7e4ca5ca6
Brian <brian.moura@indt.org.br>
2024-11-13 14:46:46 -0400

    ✨ feat: Add configuration options for Copilot including suggestion and panel setup

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,5 +3,9 @@ return {
 	config = function()
 		vim.g.copilot_no_tab_map = true
 		vim.api.nvim_set_keymap("i", "<leader><Tab>", "copilot#Accept('<CR>')", { expr=true, silent = true })
+		require("copilot").setup({
+			suggestion = { enabled = true },
+			panel = { enabled = true },
+		})
 	end,
 }
